83feba2ae04f2c2077793c4a1c585476b1df5812
Yue Zhou <yue.zhou1122@gmail.com>
2022-05-23 12:04:09 -0500

    initial commit

---
+++ test_env.py
@@ -0,0 +1,21 @@
+import sys
+import torch
+
+
+print(sys.version)
+print(sys.executable)
+
+print(f"PyTorch version: {torch.__version__}")
+
+# Check PyTorch has access to MPS (Metal Performance Shader, Apple's GPU architecture)
+print(f"Is MPS (Metal Performance Shader) built? {torch.backends.mps.is_built()}")
+print(f"Is MPS available? {torch.backends.mps.is_available()}")
+
+# Set the device
+device = "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"Using device: {device}")
+
+# Create data and send it to the device
+x = torch.rand(size=(3, 4)).to(device)
+
+print(x)
